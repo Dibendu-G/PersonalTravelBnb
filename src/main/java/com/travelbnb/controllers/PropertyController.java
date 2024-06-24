ad3f99162cbeda1435dd00658c0a948fa69026fd
dev.Dibendu <dibendughosh12@gmail.com>
2024-06-24 11:16:07 +0530

    24/6/24 code added

--- src/main/java/com/travelbnb/controllers/PropertyController.java
+++ src/main/java/com/travelbnb/controllers/PropertyController.java
@@ -1,23 +1,27 @@
 package com.travelbnb.controllers;
 
+import com.travelbnb.entity.PropertyEntity;
 import com.travelbnb.payloads.PropertyPayload;
+import com.travelbnb.repository.PropertyRepository;
 import com.travelbnb.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/property")
 public class PropertyController {
 
     private PropertyService propertyService;
+    private PropertyRepository propertyRepository;
 
     @Autowired
-    public PropertyController(PropertyService propertyService) {
+    public PropertyController(PropertyService propertyService, PropertyRepository propertyRepository) {
         this.propertyService = propertyService;
+        this.propertyRepository = propertyRepository;
     }
 
     @RequestMapping("/addProperty")
@@ -25,5 +29,12 @@ public class PropertyController {
     {
         PropertyPayload ppd = propertyService.addProperty(propertyPayload);
         return new ResponseEntity<>(ppd, HttpStatus.OK);
+    }
+
+    @GetMapping("/search/properties")
+    public ResponseEntity<List<PropertyEntity>> searchProperty(@RequestParam String name)
+    {
+        List<PropertyEntity> properties = propertyRepository.searchProperty(name);
+        return new ResponseEntity<>(properties,HttpStatus.OK);
     }
 }
--- src/main/java/com/travelbnb/repository/PropertyRepository.java
+++ src/main/java/com/travelbnb/repository/PropertyRepository.java
@@ -2,6 +2,13 @@ package com.travelbnb.repository;
 
 import com.travelbnb.entity.PropertyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PropertyRepository extends JpaRepository<PropertyEntity,Long> {
+
+    @Query("Select p from PropertyEntity p JOIN LocationEntity l on p.locationEntity=l.id where l.name=:locationName")
+    List<PropertyEntity> searchProperty(@Param("locationName") String locationName);
 }
