5ba94355a6f2f8b1e4839ddcc823631bf0655c1a
Dibendu G <68494949+Dibendu-G@users.noreply.github.com>
2024-07-12 17:49:25 +0530

    Update WebConfig.java

--- src/main/java/com/travelbnb/config/WebConfig.java
+++ src/main/java/com/travelbnb/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500") // Add your frontend URL
+                .allowedOrigins("http://127.0.0.1:5500") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
